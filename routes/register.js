4964bec193315af31436af11d44187d5beffe541
Nima Cortez <cortez.nima@gmail.com>
2023-01-11 01:01:31 +0000

    adding database query

--- routes/register.js
+++ routes/register.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const router  = express.Router();
+const { getUserByUsername, getUserByEmail, addUser } = require('./helpers');
 
-module.exports = router;
+module.exports = (db) => {
+
+// if user is not logged in
+router.get("/register", (req, res) => {
+  const templateVars = {
+    user: users[req.session],
+  };
+  res.render("register", templateVars);
+});
+
+
+return router;
+};
