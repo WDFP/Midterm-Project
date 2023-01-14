8e46e33e0f7a1aadc5e003ea7c6f906543986553
sweetmangoes <alphonsechristian@gmail.com>
2023-01-14 14:12:26 -0500

    reworked spelling

--- db/seeds/02_maps_seeds..sql
+++ db/seeds/02_maps_seeds..sql
@@ -1,7 +1,7 @@
 INSERT INTO maps(owner_id, name, description, latitude, longitude)
 VALUES (1, 'Edmonton', 'This is one of the biggest malls in North America.', '53.523293247696635', '-113.6242730114374'),
-(2, 'Duckworths Fish and chips', 'The Best Fish and Chips in GTA', '43.690093602585414', '-79.29973060803715'),
-(3, 'Rogers Centre', 'Blue Jays play ball here', '43.641966051024255', '-79.38910080045304'),
-(4, 'Parliament Hill', 'Capital City of Canada. Home of the Canadaian governing body', '45.42374428166924', '-75.70085389986698'),
+(2, 'Toronto - Eats', 'The Best Fish and Chips in GTA', '43.690093602585414', '-79.29973060803715'),
+(3, 'Toronto - sports', 'Stadiums', '43.641966051024255', '-79.38910080045304'),
+(4, 'Ottawa', 'Capital City of Canada. Home of the Canadian governing body', '45.42374428166924', '-75.70085389986698'),
 (5, 'Lighthouse Labs', 'Amazing Toronto home of Coding Bootcamps', '43.64434578279215', '-79.40219327294147');
 
